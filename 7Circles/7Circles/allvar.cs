06be11189327f5fdba036bdfb92c2dd42af1bfa2
Nuked <nuked8@gmail.com>
2012-03-25 14:05:16 +0200

    added file

---
+++ 7Circles/7Circles/allvar.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _7Circles
+{
+    public struct allvar
+    {
+        public static int xa = 0, ya = 1, za = 1;
+        public static float bgy=400;
+        public static Vector2 posizione;
+
+    }
+}
