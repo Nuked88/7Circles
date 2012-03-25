8d8f1fd94000acd1c2161bad4142d1b7fb323bc1
Nuked <nuked8@gmail.com>
2012-03-25 13:57:54 +0200

    -Fixed gravity error -Fixed velocity increase -Added Parallax Background

--- 7Circles/7Circles/Game1.cs
+++ 7Circles/7Circles/Game1.cs
@@ -25,11 +25,16 @@ namespace _7Circles
         SpriteBatch spriteBatch;
         Motion _motion;
         World world;
-        int xa = 0, ya = 1, za = 1;
+       
         Body rectangle;
         ParticleEngine particleEngine;
+            //  static background &stuff
+         Texture2D rectangleSprite, background;
 
-        Texture2D rectangleSprite, background;
+            // bg Layers
+            bgparallax bgl1;
+            bgparallax bgl2;
+      
 
         public Game1()
         {
@@ -37,9 +42,16 @@ namespace _7Circles
             {
                 _motion = new Motion();
             }
+
+           
+
+
+            //other
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             graphics.PreferredBackBufferFormat = SurfaceFormat.Color;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 480;
             graphics.IsFullScreen = true;
             graphics.SupportedOrientations = DisplayOrientation.LandscapeRight;
             // Frame rate is 30 fps by default for Windows Phone.
@@ -49,20 +61,19 @@ namespace _7Circles
             InactiveSleepTime = TimeSpan.FromSeconds(1);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+     
+
         protected override void Initialize()
-        {
+        {allvar.posizione = new Vector2(allvar.bgy, 240f);
             // TODO: Add your initialization logic here
             if (_motion != null)
             {
                 _motion.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<MotionReading>>(_motion_CurrentValueChanged);
                 _motion.Start();
             }
+            bgl1 = new bgparallax();
+            bgl2 = new bgparallax();
+
             base.Initialize();
         }
 
@@ -70,24 +81,33 @@ namespace _7Circles
         {
             rectangle.Rotation = -e.SensorReading.Attitude.Pitch;
         }
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
+
+
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             List<Texture2D> textures = new List<Texture2D>();
-            textures.Add(Content.Load<Texture2D>("circle3"));
+            //current bg
+           background = Content.Load<Texture2D>("bgbase");
+
+            //future bg          
+            bgl1.Initialize(Content, "bglayer1", GraphicsDevice.Viewport.Width, -1);
+            bgl2.Initialize(Content, "bglayer2", GraphicsDevice.Viewport.Width, -2);
+
+          
+
+
 
             particleEngine = new ParticleEngine(textures, new Vector2(200, 140));
 
             if (world == null)
             {
-                world = new World(new Vector2(xa, ya) * za);
-
+             //   world = new World(-Vector2.UnitY* allvar.za);
+                world = new World(new Vector2(allvar.xa, allvar.ya) * allvar.za);
             }
             else
             {
@@ -95,15 +115,15 @@ namespace _7Circles
             }
 
 
-
+ textures.Add(Content.Load<Texture2D>("circle3"));
             // Change Color.Red to the colour you want
             rectangle = BodyFactory.CreateCircle(world, 1f, 1f, 1.0f);
             rectangle.BodyType = BodyType.Dynamic;
-            rectangle.Position = ConvertUnits.ToSimUnits(44, 33);  // Allows the game to exit
+            rectangle.Position = ConvertUnits.ToSimUnits(44, 30);  // Allows the game to exit
 
             rectangleSprite = Content.Load<Texture2D>("circle4");
 
-            background = Content.Load<Texture2D>("firmamento3");
+            
 
 
 
@@ -129,49 +149,73 @@ namespace _7Circles
 
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            //scrolling bg
+            bgl1.Update();
+            bgl2.Update();
 
+            allvar.bgy = allvar.bgy - 0.3f;
+            allvar.posizione = new Vector2(allvar.bgy, 240f);
+       
             foreach (TouchLocation location in TouchPanel.GetState())
             {
                 rectangle.Position = ConvertUnits.ToSimUnits(location.Position.X, location.Position.Y);
-                world.ClearForces();
-                za = 0;
-                xa = 0;
-                ya = 0;
-                world = new World(new Vector2(xa, ya) * za);
-                Debug.WriteLine(world.ToString());
-            }
+                rectangle.LinearVelocity=new Vector2(0,0);
+          
+                
+                
+              //  Debug.WriteLine(rectangle.Position.ToString());
 
-            if (rectangle.Rotation >= 1)
+              }
+
+          if (rectangle.Rotation >= 1)
             {
 
-                za = -1;
-                xa = 0;
-                ya = 1;
+                allvar.za = -1;
+                allvar.xa = 0;
+                allvar.ya = 1;
 
             }
             else if (rectangle.Rotation > 0 && rectangle.Rotation < 1)
             {
-                za = 1;
-                xa = 1;
-                ya = 0;
+                allvar.za = 1;
+                allvar.xa = 1;
+                allvar.ya = 0;
             }
             else if (rectangle.Rotation <= 0)
             {
-                za = 1;
-                xa = 0;
-                ya = 1;
+                allvar.za = 1;
+                allvar.xa = 0;
+                allvar.ya = 1;
             }
-            world.Gravity = new Vector2(xa, ya) * za;
+          world.Gravity = new Vector2(allvar.xa, allvar.ya) * allvar.za;
             particleEngine.EmitterLocation = ConvertUnits.ToDisplayUnits(rectangle.Position.X, rectangle.Position.Y);
             particleEngine.Update();
 
-            /* if (rectangle.Position.Y > ConvertUnits.ToSimUnits(405))
+          if (rectangle.Position.Y > ConvertUnits.ToSimUnits(405))
              {
-                 rectangle.ApplyForce(new Vector2(0, -20));
+                 world.ClearForces();
+            
+                rectangle.ApplyForce(new Vector2(0, -20));
              }
-             */
-            world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalSeconds, (1f / 30f)));
-            // TODO: Add your update logic here
+           else if (rectangle.Position.Y <= ConvertUnits.ToSimUnits(150))
+           {
+               world.ClearForces();
+               rectangle.ApplyForce(new Vector2(0, 20));
+           }
+           else if (rectangle.Position.X > ConvertUnits.ToSimUnits(725))
+           {
+               world.ClearForces();
+               rectangle.ApplyForce(new Vector2(-20,0 ));
+           }
+           else if (rectangle.Position.X <= ConvertUnits.ToSimUnits(150))
+           {
+               world.ClearForces();
+               rectangle.ApplyForce(new Vector2(20,0 ));
+           }
+             
+          
+       
+            world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f, (1f / 30f)));
 
             base.Update(gameTime);
         }
@@ -183,17 +227,24 @@ namespace _7Circles
         protected override void Draw(GameTime gameTime)
         {
             // GraphicsDevice.Clear(Color.White);
-            spriteBatch.Begin(0, BlendState.Opaque);
+         /*   spriteBatch.Begin(0, BlendState.Opaque);
             spriteBatch.Draw(background, GraphicsDevice.Viewport.Bounds, Color.White);
-            spriteBatch.End();
-            particleEngine.Draw(spriteBatch);
-            spriteBatch.Begin();
+            spriteBatch.Draw(background, allvar.posizione, null, Color.White, 0,new Vector2(background.Width / 2.0f, background.Height / 2.0f), 1, SpriteEffects.None, 0);
+            spriteBatch.End();*/
+           spriteBatch.Begin(); 
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            
+            // Draw the moving background
+            bgl1.Draw(spriteBatch);
+            bgl2.Draw(spriteBatch);
+
+
             spriteBatch.Draw(rectangleSprite, ConvertUnits.ToDisplayUnits(rectangle.Position),
                                             null,
-                                            Color.White, rectangle.Rotation, new Vector2(rectangleSprite.Width / 2.0f, rectangleSprite.Height / 2.0f), 0.5f,
+                                            Color.White, 0, new Vector2(rectangleSprite.Width / 2.0f, rectangleSprite.Height / 2.0f), 0.5f,
                                             SpriteEffects.None, 0f);
             spriteBatch.End();
-
+particleEngine.Draw(spriteBatch);
             base.Draw(gameTime);
         }
     }
